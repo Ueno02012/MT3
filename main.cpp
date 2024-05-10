b5e735540cb22a8dc54636a4917f7ee594a5568a
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-05-10 13:02:47 +0900

    課題

--- main.cpp
+++ main.cpp
@@ -1,61 +1,19 @@
 #include <Novice.h>
 #include "Vector3.h"
+#include "Matrix.h"
 #include <cmath>
 const char kWindowTitle[] = "LC1C_04_ウエノ_ユウキ_タイトル";
 
-//加算
-Vector3 Add(const Vector3& v1, const Vector3& v2) {
-	Vector3 result{};
-	result.x = v1.x + v2.x;
-	result.y = v1.y + v2.y;
-	result.z = v1.z + v2.z;
-	return result;
-}
-//減産
-Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
-	Vector3 result{};
-	result.x = v1.x - v2.x;
-	result.y = v1.y - v2.y;
-	result.z = v1.z - v2.z;
-	return result;
-}
-//スカラー倍
-Vector3 Multiply(float scalar, const Vector3& v) {
-	Vector3 result{};
-	result.x=scalar * v.x;
-	result.y = scalar * v.y;
-	result.z = scalar * v.z;
-	return result;
-}
-//内積
-float Dot(const Vector3& v1, const Vector3& v2) {
-	Vector3 result{};
-	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-}
-//長さ
-float Length(const Vector3& v) {
-	Vector3 result{};
-	return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
-}
-//正規化
-Vector3 Nomalize(const Vector3& v) {
-	Vector3 result{};
-	result.x = v.x / Length(v);
-	result.y = v.y / Length(v);
-	result.z = v.z / Length(v);
-	return result;
-}
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
 
-
-static const int kCoumnWidth = 60;
-void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
-	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
-	Novice::ScreenPrintf(x + kCoumnWidth, y, "%.02f", vector.y);
-	Novice::ScreenPrintf(x + kCoumnWidth * 2, y, "%.02f", vector.z);
-	Novice::ScreenPrintf(x + kCoumnWidth * 3, y, "%s", label);
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix) {
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
 }
-
-
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -63,12 +21,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
-
-	int kRowHeight = 20;
-	Vector3 v1{ 1.0f,3.0f,-5.0f };
-	Vector3 v2{ 4.0f,-1.0f,2.0f };
-	float k = { 4.0f };
-
+	Matrix4x4 m1 = {
+		3.2f,0.7f,9.6f,4.4f,
+		5.5f,1.3f,7.8f,2.1f,
+		6.9f,8.0f,2.6f,1.0f,
+		0.5f,7.2f,5.1f,3.3f
+	};
+	
+	Matrix4x4 m2 = {
+		4.1f,6.5f,3.3f,2.2f,
+		8.8f,0.6f,9.9f,7.7f,
+		1.1f,5.5f,6.6f,0.0f,
+		3.3f,9.9f,8.8f,2.2f
+	};
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -87,13 +52,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		Vector3 resultAdd = Add(v1, v2);
-		Vector3 resultSubtract = Subtract(v1, v2);
-		Vector3 resultMultiply = Multiply(k, v1);
-		float resultDot = Dot(v1, v2);
-		float resultLength = Length(v1);
-		Vector3 resultNormalize = Nomalize(v2);
+		Matrix4x4 resultAdd = Add(m1, m2);
+		Matrix4x4 resultMultiply = Multiply(m1, m2);
+		Matrix4x4 resultSubtract = Subtract(m1, m2);
+		Matrix4x4 inverseM1 = Inverse(m1);
+		Matrix4x4 inverseM2 = Inverse(m2);
+		Matrix4x4 transposeM1 = Transpose(m1);
+		Matrix4x4 transposeM2 = Transpose(m2);
+		Matrix4x4 identity = MakeIdentity();
 
+		
 
 		///
 		/// ↑更新処理ここまで
@@ -102,13 +70,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-
-		VectorScreenPrintf(0, 0, resultAdd, ": Add");
-		VectorScreenPrintf(0, kRowHeight, resultSubtract, ": Subtract");
-		VectorScreenPrintf(0, kRowHeight*2, resultMultiply, ": Multiply");
-		Novice::ScreenPrintf(0, kRowHeight*3, "%.02f : Dot", resultDot);
-		Novice::ScreenPrintf(0, kRowHeight*4, "%.02f : Length", resultLength);
-		VectorScreenPrintf(0, kRowHeight *5 , resultNormalize, ": Normalize");
+		
+		MatrixScreenPrintf(0, 0, resultAdd);
+		MatrixScreenPrintf(0, kRowHeight * 5, resultSubtract);
+		MatrixScreenPrintf(0, kRowHeight * 5 * 2, resultMultiply);
+		MatrixScreenPrintf(0, kRowHeight * 5 * 3, inverseM1);
+		MatrixScreenPrintf(0, kRowHeight * 5 * 4, inverseM2);
+		MatrixScreenPrintf(kColumnWidth * 5, 0, transposeM1);
+		MatrixScreenPrintf(kColumnWidth * 5, kRowHeight * 5, transposeM2);
+		MatrixScreenPrintf(kColumnWidth * 5,kRowHeight*5*2,identity);
 
 
 

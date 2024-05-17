7b928706b68c1131021d0c6aad9173c995996419
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-05-17 14:34:09 +0900

    確認課題

--- Matrix.h
+++ Matrix.h
@@ -2,6 +2,22 @@
 #include "Vector3.h"
 #include "Matrix4x4.h"
 #include <cassert>
+#include <cmath>
+
+
+// 行列の積
+static Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 result{};
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			for (int k = 0; k < 4; k++) {
+				result.m[i][j] += m1.m[i][k] * m2.m[k][j];
+			}
+		}
+	}
+	return result;
+
+}
 
 // 1. X軸の回転行列
 static Matrix4x4 MakeRotateXMatrix(float radian) {
--- main.cpp
+++ main.cpp
@@ -28,11 +28,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
 
-	Vector3 rotate{ 0.4f,1.43f,-0.0f };
+	Vector3 rotate{ 0.4f,1.43f,-0.8f };
+
 	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
 	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
 	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
-	Matrix4x4 rotateXYZMatrix=
+	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
 
 
 
@@ -63,7 +64,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 		
-
+		MatrixScreenPrintf(0, 0, rotateXMatrix);
+		MatrixScreenPrintf(0, kRowHeight * 5, rotateYMatrix);
+		MatrixScreenPrintf(0, kRowHeight * 5 * 2, rotateZMatrix);
+		MatrixScreenPrintf(0, kRowHeight * 5 * 3, rotateXYZMatrix);
 
 		///
 		/// ↑描画処理ここまで

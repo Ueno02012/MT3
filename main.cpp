0252f513da9989c8f57ba6786c0bbf2a577de9ea
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-05-24 10:12:41 +0900

    test

--- main.cpp
+++ main.cpp
@@ -26,16 +26,8 @@ void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
 
-	Matrix4x4 orthographicMatrix = 
-		MakeOrthographicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);
-
-	Matrix4x4 perspectiveFovMatrix =
-		MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
-
-	Matrix4x4 viewportMatrix =
-		MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
+	Matrix4x4 worldMatrix=MakeAffineMatrix()
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -64,9 +56,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 		
-		MatrixScreenPrintf(0, 0, orthographicMatrix,"orthographicMatrix");
-		MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix,"perspectiveFovMatrix");
-		MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix,"viewportMatrix");
 
 		///
 		/// ↑描画処理ここまで
